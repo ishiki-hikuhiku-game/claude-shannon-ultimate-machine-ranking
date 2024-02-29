629d2685335f604a6b8a6e9fcb76fb16375840f3
Tanaka Kensaku <tannakaken@gmail.com>
2024-02-29 22:15:38 +0900

    本番環境のORIGINを追加

--- src/helpers/api.helper.ts
+++ src/helpers/api.helper.ts
@@ -3,7 +3,7 @@
  * TODO 本番用のORIGINも設定する
  */
 export const CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "http://localhost:55555",
+    "Access-Control-Allow-Origin": "http://localhost:55555,https://claude-shannons-ultimate-machine.netlify.app",
     "Access-Control-Allow-Methods": "POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
   } as const;
